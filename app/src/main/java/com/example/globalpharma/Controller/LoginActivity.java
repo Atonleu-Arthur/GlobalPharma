3fc1ee33e484571883cf7c083de68cacf68d45a7
audreykemamen <52709353+audreykemamen@users.noreply.github.com>
2020-02-29 09:13:54 +0100

    Amélioration de l'interface de connexion : ajout du bouton de connexion Ajout des interactions

--- app/src/main/java/com/example/globalpharma/Controller/LoginActivity.java
+++ app/src/main/java/com/example/globalpharma/Controller/LoginActivity.java
@@ -2,15 +2,36 @@ package com.example.globalpharma.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.globalpharma.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    private Button mBtnRegistration;
+    private Button mBtnRegister;
+    private Button mBtnConnection;
+    private Button mBtnConnect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mBtnRegistration = findViewById(R.id.btnRegistration);
+
+        mBtnConnect = findViewById(R.id.btnSubmit);
+        mBtnConnect.setEnabled(false);
+        mBtnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/globalpharma/Controller/RegisterActivity.java
+++ app/src/main/java/com/example/globalpharma/Controller/RegisterActivity.java
@@ -2,15 +2,31 @@ package com.example.globalpharma.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.globalpharma.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button mBtnConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        mBtnConnection = findViewById(R.id.btnConnect);
+
+        mBtnConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
