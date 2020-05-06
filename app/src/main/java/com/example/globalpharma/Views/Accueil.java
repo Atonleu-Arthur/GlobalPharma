7737211bea2c016bc68e469a51ea5eccb14750e4
Atonleu-Arthur <56762615+Atonleu-Arthur@users.noreply.github.com>
2020-05-06 12:40:47 +0200

    Merge branch 'Atonleu' of https://github.com/Atonleu-Arthur/GlobalPharma into ATonleu
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/globalpharma/Controller/LoginActivity.java
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/globalpharma/Views/Accueil.java
+++ app/src/main/java/com/example/globalpharma/Views/Accueil.java
@@ -39,7 +39,8 @@ public class Accueil extends  AppCompatActivity implements BottomNavigationView.
                 break;
 
             case R.id.pills:
-                fragment = new PillsFragment();
+               fragment = new PillsFragment();
+
 
                 break;
 
--- app/src/main/java/com/example/globalpharma/Views/GpsFragment.java
+++ app/src/main/java/com/example/globalpharma/Views/GpsFragment.java
@@ -18,10 +18,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.globalpharma.Common;
-import com.example.globalpharma.IGoogleAPIService;
-import com.example.globalpharma.Model.MyPlaces;
-import com.example.globalpharma.Model.Results;
+import com.example.globalpharma.GetNearbyPlacesData;
 import com.example.globalpharma.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -37,30 +34,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link GpsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+/**public class GpsFragment extends Fragment implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {**/
 public class GpsFragment extends Fragment implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static final int MY_PERMISSION_CODE = 1000;
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
+  private String mParam1;
     private String mParam2;
-    GoogleMap mMap;
+    /**   GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
-    SupportMapFragment mapFragment;
+
     Button button;
     private double lattitude , longitude;
     private Location mLastConnection;
@@ -68,14 +64,26 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback,
     private LocationRequest mLocationRequest;
     IGoogleAPIService mService;
     private Button Pharmacieprox;
-
+**/
+// cODE DE LA DEUXIEME METHODE
+    SupportMapFragment mapFragment;
+   private GoogleMap mMap;
+    private GoogleApiClient client;
+    private LocationRequest locationRequest;
+    private Location lastlocation;
+    private Marker currentLocationmMarker;
+    public static final int REQUEST_LOCATION_CODE = 99;
+    int PROXIMITY_RADIUS = 10000;
+    double latitude,longitude;
+    Button btnpharmacie ;
+    Object dataTransfer[] = new Object[2];
+    GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
 
     public GpsFragment() {
         // Required empty public constructor
     }
 
 
-    // TODO: Rename and change types and number of parameters
     public static GpsFragment newInstance(String param1, String param2) {
         GpsFragment fragment = new GpsFragment();
         Bundle args = new Bundle();
@@ -84,24 +92,230 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback,
         fragment.setArguments(args);
         return fragment;
     }
-
+// changement
     @Override
     public void onCreate  (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-            mService= Common.getGoogleAPIService();
-            Pharmacieprox.findViewById(R.id.btnPharmacie);
-            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-                checkLocationPermission();
-            }
-            Pharmacieprox.setOnClickListener(v -> nearByPlace("Pharmacie"));
 
+            /** mService= Common.getGoogleAPIService();
+            Pharmacieprox.findViewById(R.id.btnPharmacie);
+          if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                /**checkLocationPermission();
+                nearByPlace("Pharmacie");
+
+            }
+**/
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                checkLocationPermission();
+
+            }
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            mapFragment=(SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googlemap);
+            mapFragment.getMapAsync(this);
+
+
+        }
+
+
+    }
+
+    public void LocalPharmacieP()
+    {
+
+        btnpharmacie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.clear();
+                String Pharmacie = "Pharmacy";
+                String url = getUrl(latitude, longitude, Pharmacie);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(getContext(), "Pharmacie a Proximite", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(100);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
         }
     }
 
-    private void nearByPlace(final String placeType) {
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+   /** public void onClick(View v)
+    {
+        Object dataTransfer[] = new Object[2];
+        GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+
+        switch(v.getId())
+        {
+          /** case R.id.B_search:
+                EditText tf_location =  findViewById(R.id.TF_location);
+                String location = tf_location.getText().toString();
+                List<Address> addressList;
+
+
+                if(!location.equals(""))
+                {
+                    Geocoder geocoder = new Geocoder(getContext());
+
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 5);
+
+                        if(addressList != null)
+                        {
+                            for(int i = 0;i<addressList.size();i++)
+                            {
+                                LatLng latLng = new LatLng(addressList.get(i).getLatitude() , addressList.get(i).getLongitude());
+                                MarkerOptions markerOptions = new MarkerOptions();
+                                markerOptions.position(latLng);
+                                markerOptions.title(location);
+                                mMap.addMarker(markerOptions);
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;**/
+           /** case R.id.btnPharmacie:
+                mMap.clear();
+                String hospital = "Pharmacie";
+                String url = getUrl(latitude, longitude, hospital);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(getContext(), "Showing Nearby pharmacy", Toast.LENGTH_SHORT).show();
+                break;
+
+
+     /**       case R.id.:
+                mMap.clear();
+                String school = "Pharmacie";
+                url = getUrl(latitude, longitude, school);
+                dataTransfer[0] = mMap;
+                dataTransfer[1] = url;
+
+                getNearbyPlacesData.execute(dataTransfer);
+                Toast.makeText(getContext(), "Showing Nearby pharmacy", Toast.LENGTH_SHORT).show();
+                break;**/
+
+      /**  }
+    }**/
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        lastlocation = location;
+        if(currentLocationmMarker != null)
+        {
+            currentLocationmMarker.remove();
+
+        }
+        Log.d("lat = ",""+latitude);
+        LatLng latLng = new LatLng(location.getLatitude() , location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Ma position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        currentLocationmMarker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+
+        if(client != null)
+        {
+            LocationServices.FusedLocationApi.removeLocationUpdates(client,this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            bulidGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+    protected synchronized void bulidGoogleApiClient() {
+        client = new GoogleApiClient.Builder(getContext()).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        client.connect();
+
+    }
+
+    private String getUrl(double latitude , double longitude , String nearbyPlace)
+    {
+
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location="+latitude+","+longitude);
+        googlePlaceUrl.append("&radius="+PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type="+ nearbyPlace);
+        googlePlaceUrl.append("&sensor=true");
+        googlePlaceUrl.append("&key="+"AIzaSyAhHG39XMYaWOlyzMe-_KBlAXUpRabjmNg");
+        Log.d("GpsFragment", "url = "+googlePlaceUrl.toString());
+
+        return googlePlaceUrl.toString();
+    }
+    public boolean checkLocationPermission()
+    {
+        if(ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED )
+        {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
+            }
+            return false;
+
+        }
+        else
+            return true;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_gps, container, false);
+        btnpharmacie=(Button) v.findViewById(R.id.btnPharmacie);
+        LocalPharmacieP();
+        mapFragment=(SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googlemap);
+        mapFragment.getMapAsync(this);
+
+        return v;
+    }
+
+    /** private void nearByPlace(final String placeType) {
         mMap.clear();
         String url=getUrl (lattitude,longitude,placeType);
         mService.getNearByPlaces(url)
@@ -124,8 +338,10 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback,
                                 if(placeType.equals("Pharmacie"))
                                     markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.pharmacy));
                                 //Pour l'implementation des pharmacie des gardes
+                                else
+                                   markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
                                 /**  else if(placeType.equals("Pharmacie"))
-                                 markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.pharmacy));**/
+                                 markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.pharmacy));
                                 mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
                                 mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
                             }
@@ -141,11 +357,13 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback,
 
     private String getUrl(double lattitude, double longitude, String placeType) {
         StringBuilder googlePlaceUrl =new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
-        googlePlaceUrl.append("localisation"+lattitude+longitude);
+        googlePlaceUrl.append("localisation"+lattitude+","+longitude);
+        googlePlaceUrl.append("&radius="+10000);
         googlePlaceUrl.append("&type="+placeType);
         googlePlaceUrl.append("&sensor=true");
         googlePlaceUrl.append("&key="+getResources().getString(R.string.Apiplaces));
         Log.d("getUrl",googlePlaceUrl.toString());
+
         return googlePlaceUrl.toString();
     }
 
@@ -278,5 +496,6 @@ public class GpsFragment extends Fragment implements OnMapReadyCallback,
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
     }
 
-
+**/
 }
+
